355bc8994560b30ec727843892d0c4cd344a35c3
Nathan <eyerleyn@gmail.com>
2017-08-21 22:26:26 +0000

    first push, basic styling templates and fonts created, also a login screen template

---
+++ screens/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Text } from '../../styles/';
+
+export default function Login(props) {
+  return (
+    <Container>
+      <Text h4>Sign in to start blogging</Text>
+    </Container>
+    );
+}
